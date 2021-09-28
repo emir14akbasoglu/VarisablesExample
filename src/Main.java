439ef1c106a9c834164f1368ac207b3b3fba4543
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 17:09:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main (String[] args) {
+		// TODO Auto-generated method stub
+		String phoneName="Redmi note9 128 GB";
+		double phonePrice=3039.05;
+		double phoneStar=4.7;
+	    String phoneIsDiscound="Green";
+	    int phoneDiscound=5;
+	    String phoneweight="199"; 
+	    String pohoneconnection="42.2";
+	    String batterypowerRange="5020";
+	    String pohonecamera="48";
+	    
+	    
+	    system.out.println("telefon adý:"+phonename);
+	    
+		
+		
+		
+		
+
+	}
+
+}
